ffac4f7f992cc1bda1d20a11bdf347ac6b62e6a7
davidslayin <dave191994@gmail.com>
2022-12-21 08:29:55 -0800

    7-multi_c.js

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,5 +1,6 @@
 #!/usr/bin/node
-const multi_ling = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
-for (const value of multi_ling) {
-	console.log(value);
+const multiLang = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+
+for (const value of multiLang) {
+  console.log(value);
 }
