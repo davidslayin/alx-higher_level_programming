b9e6aec633de1d2847ccd70deb334d79b9beb88f
davidslayin <dave191994@gmail.com>
2023-03-14 00:29:20 +0300

    Delete 0x12-javascript-warm_up directory

--- 0x12-javascript-warm_up/3-value_argument.js
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/node
-const arg = process.argv[2];
-if (arg) {
-  console.log(arg);
-} else {
-  console.log('No argument');
-}
--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/node
-const multiLang = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
-
-for (const value of multiLang) {
-  console.log(value);
-}
