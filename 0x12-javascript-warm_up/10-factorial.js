ebec9742e83ad98ad6a65d8b212c80f142c178ff
davidslayin <dave191994@gmail.com>
2023-03-13 16:48:56 -0700

    tasks till task 14

---
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+const args = parseInt(process.argv[2]);
+
+function factorial(a) {
+  if (isNaN(a)) {
+    return 1;
+  }
+  return a * factorial(a - 1);
+}
+
+console.log(factorial(args));
