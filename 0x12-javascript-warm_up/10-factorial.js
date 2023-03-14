b2c8f70aae4120699e5111a759bbb582683d4ae5
davidslayin <dave191994@gmail.com>
2023-03-13 17:11:08 -0700

    fixing and task 15

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -1,8 +1,8 @@
 #!/usr/bin/node
 const args = parseInt(process.argv[2]);
 
-function factorial(a) {
-  if (isNaN(a)) {
+function factorial (a) {
+  if (isNaN(a) || a <= 0) {
     return 1;
   }
   return a * factorial(a - 1);
